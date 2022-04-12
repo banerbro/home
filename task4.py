1be6a74f252347a0d16ae14946f6fa3f10e5a2e9
banerbro <103130461+banerbro@users.noreply.github.com>
2022-04-12 14:49:48 +0300

    Add files via upload

--- task4.py
+++ task4.py
@@ -1,11 +1,12 @@
-def mathhelp():
-    z = int(input('Угол'))
-    x = radians(cos(z))
-    c = radians(sin(z))
-    v = radians(tan(z))
-    b = radians(ctan(z))
-    print('Косинус', x)
-    print('Синус', c)
-    print('Тангенс', v)
-    print('Котангенс', b)
-mathhelp()
+import math
+z = int(input('дите градус угла:'))
+n = math.radians(z)
+x = math.cos(n)
+x= math.degrees(x)
+c = math.sin(n)
+c = math.degrees(c)
+v = math.tan(n)
+v = math.degrees(v)
+b = x/c
+b = math.degrees(b)
+print('синус,косинус,тангенс,котангенс:', x, c, v, b)
